c2c8d3ff4b33af934894a34b52b74a63fa52ed17
adib82302 <adib82302@gmail.com>
2024-04-26 05:43:22 -0400

    black and white image detection

---
+++ Computer_Vision/black_and_white.py
@@ -0,0 +1,45 @@
+from ultralytics import YOLO
+import cv2
+import math
+
+# Settings
+video_path = '/Users/adibk/Downloads/20240209_130940A.mp4'
+cap = cv2.VideoCapture(video_path)
+model = YOLO('/Users/adibk/Documents/Senior-Design-Project/Computer_Vision/best.pt')
+
+# Define text color
+text_color = (0, 255, 0)  # Green color for confidence interval text
+
+while cap.isOpened():
+    video_feed, frame = cap.read()
+    
+    #Pre-processing
+    if not video_feed:
+        print("cannot open file to process video ...  exiting")
+        break
+    
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #Processing
+    still_images = model(frame, stream=True)
+    for image in still_images:
+        boxes = image.boxes
+        for box in boxes:
+            # Bounding box
+            x1, y1, x2, y2 = box.xyxy[0] 
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(gray, (x1, y1), (x2, y2),(0,0,255), 5)
+            
+            # Confidence
+            conf = math.ceil((box.conf[0]*100))/100
+            cv2.putText(gray, f'{conf}', (x1, y1-20), cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 5)
+
+    #Post-Processing
+    cv2.imshow("Video Feed with Detection", gray)
+    if cv2.waitKey(1) == ord('f'):
+        break
+
+#Shut-down Procedure
+cap.release()
+cv2.destroyAllWindows()
